983f127eba55f8710006a95221d664b7458afeed
Sean Harrington <sean@never-not.com>
2018-01-27 13:40:40 -0500

    Import my scene and scripts
    Signed-off-by: Sean Harrington <sean@never-not.com>

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("down")){
+			transform.Translate(0,1,0);
+		}
+		if (Input.GetButton("up")){
+			transform.Translate(0,-1,0);
+		}
+		if (Input.GetButton("left")){
+			transform.Translate(-1,0,0);
+		}
+		if (Input.GetButton("right")){
+			transform.Translate(1,0,0);
+		}
+	}
+}
