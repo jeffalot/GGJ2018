aefe099776c4ede206cd1b195c9cef9ac5b957c4
Sean Harrington <sean@never-not.com>
2018-01-28 13:15:39 -0500

    Added billboard, WIP GameController
    Signed-off-by: Sean Harrington <sean@never-not.com>

--- Assets/Scripts/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class CameraBehavior : MonoBehaviour {
 
-	public GameObject player;
 	public GameObject cameraBoundsMin;
 	public GameObject cameraBoundsMax;
+
+	private GameObject player;
+
+	/// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void Start()
+	{
+		player = GameObject.FindWithTag("Player");	
+	}
 	
 	// Update is called once per frame
 	void Update () {
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text scoreText;
+	public Text infectedText;
+    public Text restartText;
+    public Text gameOverText;
+
+    private bool gameOver;
+    private bool restart;
+    private int score;
+	private int infected;
+
+	public GameObject[] goals;
+	public GameObject homeBase;
+
+	private GameObject currentGoal;
+
+    void Start ()
+    {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        score = 0;
+		infected = 0;
+        UpdateScore ();
+		getNewGoal();
+    }
+ 
+    void Update ()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown (KeyCode.R))
+            {
+                Application.LoadLevel (Application.loadedLevel);
+            }
+        }
+
+		checkForRestart();
+
+		updateInfected();
+    }
+
+	public void getNewGoal(){
+		currentGoal = goals[Random.Range(0,goals.Length)];
+	}
+
+	void checkForRestart(){
+		if (gameOver)
+		{
+			restartText.text = "Press 'R' for Restart";
+			restart = true;
+		}
+	}
+
+	void UpdateInfected(){
+		infected = GameObject.FindGameObjectsWithTag("Infected").Length;
+		infectedText = "Infected: " + infected;
+	}
+
+    public void AddScore (int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore ();
+    }
+
+    void UpdateScore ()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver ()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+}
