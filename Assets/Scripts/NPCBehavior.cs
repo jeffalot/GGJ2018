5bb2cac3bd2df5fdabf54192fb23729ef5492f67
Jeffalot <jah5fv@virginia.edu>
2018-01-27 19:15:08 -0500

    Testing with Character Walk Paths, Added Box Testing Scene

---
+++ Assets/Scripts/NPCBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCBehavior : MonoBehaviour {
+
+
+    public float speed = 100f;
+    public int freq = 2;
+    public int amp = 2;
+
+    public float seed = 0f;
+
+	// Use this for initialization
+	void Start () {
+
+        seed = Random.Range(0f, 1000f);
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+
+        var pos = transform.position;
+        pos.x += Mathf.Sin(freq * (Time.time + seed) ) * amp;  //speed* Time.deltaTime;
+        
+        transform.position = pos;
+
+        
+
+    }
+}
