254ad0d7a2ed42cbb27384901f056645839d2571
Sean Harrington <sean@never-not.com>
2018-01-27 20:42:41 -0500

    Add Camera Boundaries to Main Scene
    Signed-off-by: Sean Harrington <sean@never-not.com>

--- Assets/Scripts/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class CameraBehavior : MonoBehaviour {
 
 	public GameObject player;
-	// Transform tempT;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+	public GameObject cameraBoundsMin;
+	public GameObject cameraBoundsMax;
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = new Vector3(
+		float horzExtent = Camera.main.orthographicSize * Screen.width / Screen.height;
+		float newCameraPosX = Mathf.Clamp(
 			player.transform.position.x, 
+			cameraBoundsMin.transform.position.x,
+			cameraBoundsMax.transform.position.x
+		);
+
+		Debug.Log(cameraBoundsMin.transform.position.x);
+		Debug.Log(cameraBoundsMax.transform.position.x);
+
+		transform.position = new Vector3(
+			newCameraPosX, 
 			transform.position.y,
 			transform.position.z
 		);
